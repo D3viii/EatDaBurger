3ef91c9029936789637654f4ff288a5227a51156
D3viii <devinsdevs@gmail.com>
2020-04-15 17:43:30 -0400

    Hopefully Works now

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_name, devoured) VALUES ('plain jane', true);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('loaded', true);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('veggie', false);
